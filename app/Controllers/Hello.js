f6d1db91ceea82d28707cb55e46f29a7bd7df5bc
Paulo Leonardo <39014913+paulo-leo@users.noreply.github.com>
2024-05-02 09:53:51 -0300

    Update Hello.js

--- app/Controllers/Hello.js
+++ app/Controllers/Hello.js
@@ -1,3 +1,8 @@
+/*
+Esta é uma classe controladora simples de teste que exibe a mensagem 'Hello World!!!' ao 
+iniciar o seu aplicativo. O objetivo desta classe é demonstrar de forma prática 
+e simples como as integrações IPC já estão configuradas.
+*/
 export default class Hello {
 
     print(request) {
